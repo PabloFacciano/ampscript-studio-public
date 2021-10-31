90ecf3885a8df9c6be38570049582974a3130038
Pablo Facciano <pabloezequielfacha@gmail.com>
2021-10-31 02:26:13 -0300

    Load workspaces on signin

--- src/supabase.js
+++ src/supabase.js
@@ -9,8 +9,11 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey)
 supabase.auth.onAuthStateChange((event, session) => {
   console.log(event, session)
   if (event == 'SIGNED_IN'){
+    // Login is now logged in
     store.commit('setUser',session.user);
+    store.dispatch('loadWorkspaces');
   } else if (event == 'SIGNED_OUT'){
+    // User is gone
     store.commit('setUser',null);
   }
 
